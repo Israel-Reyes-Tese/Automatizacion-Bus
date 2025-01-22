24713577e508b745203a3e2d091b4a54424b28bd
ing.israel <85515018+Israel-Reyes-Tese@users.noreply.github.com>
2025-01-22 11:40:54 -0600

    Se resume el archivo FILE_HAND

--- herramientas/Archivos_temporales/Agentes_temporales/agente_snmp/ABR/FILE_HANDLER.pm
+++ herramientas/Archivos_temporales/Agentes_temporales/agente_snmp/ABR/FILE_HANDLER.pm
@@ -1,28 +1,62 @@
 package ABR::FILE_HANDLER;
-# Version=1.0
 use warnings;
 use strict;
 
 sub new {
     my $class = shift;
-    my $self = {};
-    bless $self, $class;
-    return $self;
-}
+    my $args = { @_ };
+    my $gluster_dir = $args->{gluster_dir};
+    my $auxiliary_dir = $args->{auxiliary_dir};
 
-sub read_file {
-    my ($self, $file_path) = MainWindow=HASH(0x19ca5c3d4f0) agente_snmp herramientas\Archivos_temporales\Agentes_temporales\agente_snmp\ABR;
-    open my $fh, '<', $file_path or die "No se puede abrir el archivo: $!";
-    my @lines = <GLOB(0x19ca5d61270)>;
-    close $fh;
-    return @lines;
+    return bless { gluster_dir => $gluster_dir, auxiliary_dir => $auxiliary_dir, not_send => [] }, $class;
 }
 
 sub write_file {
-    my ($self, $file_path, @lines) = MainWindow=HASH(0x19ca5c3d4f0) agente_snmp herramientas\Archivos_temporales\Agentes_temporales\agente_snmp\ABR;
-    open my $fh, '>', $file_path or die "No se puede abrir el archivo: $!";
-    print $fh @lines;
+    my ($self, $file_name, $file_content) = MainWindow=HASH(0x294de083f40) agente_snmp herramientas\Archivos_temporales\Agentes_temporales\agente_snmp\ABR;
+    my $file_path = "$self->{gluster_dir}/$file_name";
+    my $emergency_path = "$self->{auxiliary_dir}/$file_name";
+
+    unless (open my $fh, '>', $file_path) {
+        unless (open my $em_fh, '>', $emergency_path) {
+            die "The emergency directory does not exist or is not accessible";
+        }
+        print $em_fh $file_content;
+        close $em_fh;
+        push @{$self->{not_send}}, $file_name;
+        return "EMERGENCY";
+    }
+
+    $self->file_resynch if @{$self->{not_send}};
+    print $fh $file_content;
     close $fh;
+    return "WRITTEN";
+}
+
+sub file_resynch {
+    my $self = shift;
+    while (my $emergency_file = shift @{$self->{not_send}}) {
+        my $file_path = "$self->{gluster_dir}/$emergency_file";
+        my $emergency_path = "$self->{auxiliary_dir}/$emergency_file";
+        unless (system("mv -f $emergency_path $file_path") == 0) {
+            unshift @{$self->{not_send}}, $emergency_file;
+            last;
+        }
+    }
+}
+
+sub dummy_write {
+    my $self = shift;
+    $self->file_resynch if @{$self->{not_send}} && opendir my $dir, $self->{gluster_dir};
+}
+
+sub startup_write {
+    my $self = shift;
+    if (opendir my $dir, $self->{gluster_dir}) {
+        my $regreso = `ls -rt $self->{auxiliary_dir}`;
+        die "There is a problem reading files from the emergency file path" if $? == -1;
+        @{$self->{not_send}} = split "\n", $regreso;
+        $self->file_resynch;
+    }
 }
 
 1;
--- herramientas/LogicEstructura.pm
+++ herramientas/LogicEstructura.pm
@@ -1010,6 +1010,93 @@ END_CODE
     return 1;
 }
 
+sub crear_file_handler {
+    my ($ventana_principal, $agente, $ruta_agente_abr) = @_;
+
+    my $archivo_file_handler = File::Spec->catfile($ruta_agente_abr, "FILE_HANDLER.pm");
+
+    if (-e $archivo_file_handler) {
+        open my $fh, '>', $archivo_file_handler or do {
+            herramientas::Complementos::show_alert($ventana_principal, 'ERROR', "No se puede abrir el archivo: $!", 'error');
+            return;
+        };
+        close $fh;
+    }
+
+    open my $fh, '>', $archivo_file_handler or do {
+        herramientas::Complementos::show_alert($ventana_principal, 'ERROR', "No se puede abrir el archivo: $!", 'error');
+        return;
+    };
+
+    print $fh <<"END_CODE";
+package ABR::FILE_HANDLER;
+use warnings;
+use strict;
+
+sub new {
+    my \$class = shift;
+    my \$args = { \@_ };
+    my \$gluster_dir = \$args->{gluster_dir};
+    my \$auxiliary_dir = \$args->{auxiliary_dir};
+
+    return bless { gluster_dir => \$gluster_dir, auxiliary_dir => \$auxiliary_dir, not_send => [] }, \$class;
+}
+
+sub write_file {
+    my (\$self, \$file_name, \$file_content) = @_;
+    my \$file_path = "\$self->{gluster_dir}/\$file_name";
+    my \$emergency_path = "\$self->{auxiliary_dir}/\$file_name";
+
+    unless (open my \$fh, '>', \$file_path) {
+        unless (open my \$em_fh, '>', \$emergency_path) {
+            die "The emergency directory does not exist or is not accessible";
+        }
+        print \$em_fh \$file_content;
+        close \$em_fh;
+        push \@{\$self->{not_send}}, \$file_name;
+        return "EMERGENCY";
+    }
+
+    \$self->file_resynch if \@{\$self->{not_send}};
+    print \$fh \$file_content;
+    close \$fh;
+    return "WRITTEN";
+}
+
+sub file_resynch {
+    my \$self = shift;
+    while (my \$emergency_file = shift \@{\$self->{not_send}}) {
+        my \$file_path = "\$self->{gluster_dir}/\$emergency_file";
+        my \$emergency_path = "\$self->{auxiliary_dir}/\$emergency_file";
+        unless (system("mv -f \$emergency_path \$file_path") == 0) {
+            unshift \@{\$self->{not_send}}, \$emergency_file;
+            last;
+        }
+    }
+}
+
+sub dummy_write {
+    my \$self = shift;
+    \$self->file_resynch if \@{\$self->{not_send}} && opendir my \$dir, \$self->{gluster_dir};
+}
+
+sub startup_write {
+    my \$self = shift;
+    if (opendir my \$dir, \$self->{gluster_dir}) {
+        my \$regreso = `ls -rt \$self->{auxiliary_dir}`;
+        die "There is a problem reading files from the emergency file path" if \$? == -1;
+        \@{\$self->{not_send}} = split "\\n", \$regreso;
+        \$self->file_resynch;
+    }
+}
+
+1;
+END_CODE
+
+    close $fh;
+    herramientas::Complementos::show_alert($ventana_principal, 'EXITO', "Se creo correctamente el archivo $archivo_file_handler", 'success');
+    return 1;
+}
 
 
 1;
