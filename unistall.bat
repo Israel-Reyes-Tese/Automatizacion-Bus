f609ecb35e143b13d26d6c122a83117f181d40f9
ing.israel <85515018+Israel-Reyes-Tese@users.noreply.github.com>
2025-02-13 13:55:49 -0600

    Se actualiza el archivo instalador para no sea necesario instalar librerias

---
+++ unistall.bat
@@ -0,0 +1,5 @@
+@echo off
+REM 1. Desinstalar módulos desde requirements.txt
+powershell -Command "Get-Content requirements.txt | Where-Object { $_ -notmatch '^\s*#' -and $_ -match 'cpanm' } | ForEach-Object { $_.Split(' ')[1] } | ForEach-Object { cpanm --uninstall $_ }"
+
+echo ¡Parche aplicado en %filepath%!
