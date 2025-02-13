eaa5f79eb3fcc4114540ee9da5be9b715fab92d0
ing.israel <85515018+Israel-Reyes-Tese@users.noreply.github.com>
2025-02-13 16:04:06 -0600

    Se agrega la preparacion de parches y crea una nueva carpeta donde alberga los mismos

--- install.bat
+++ install.bat
@@ -11,7 +11,7 @@ REM 1. Instalar módulos desde requirements.txt
 powershell -Command "Get-Content requirements.txt | Where-Object { $_ -notmatch '^\s*#' -and $_ -match 'cpanm' } | ForEach-Object { $_.Split(' ')[1] } | ForEach-Object { cpanm $_ }"
 
 REM 2. Buscar ruta de Tk/FileDialog.pm y aplicar parche
-set filepath=C:/Strawberry/perl/site/lib/Tk/FileDialog.pm
+set filepath=C:\Strawberry\perl\site\lib\Tk\FileDialog.pm
 set patchfile=herramientas\Archivos_temporales\Parche\Librerias\FileDialog.pm
 
 if not exist "%filepath%" (
@@ -25,7 +25,7 @@ copy /Y "%patchfile%" "%filepath%"
 if %errorlevel% neq 0 (
     echo Error: No se pudo aplicar el parche automaticamente.
     powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Favor de copiar y reemplazar el archivo FileDialog.pm de Parche librerias a la carpeta raiz de las librerias.', 'Aviso', 'OK', 'Warning')"
-    start explorer "C:/Strawberry/perl/site/lib/Tk/"
+    start explorer "C:\Strawberry\perl\site\lib\Tk\"
     start explorer "herramientas\Archivos_temporales\Parche\Librerias\"
     exit /b 1
 )
